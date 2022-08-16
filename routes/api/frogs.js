5a87418fee094ebef92d672a6ecbc9cffb47c842
Tiffany Domugho <tiffany@domugho.com>
2022-08-16 23:02:51 +0800

    chore: correct header path

--- routes/api/frogs.js
+++ routes/api/frogs.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const headers = require('../middleware/headers')
+const headers = require('../../middleware/headers')
 
 let frogs = require('../../frogs_metadata.js');
 
