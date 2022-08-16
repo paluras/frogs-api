563c0952e2d24690e3aff8b0c10a741ecfcf1f44
Tiffany Domugho <tiffany@domugho.com>
2022-08-16 22:59:44 +0800

    chore: add headers

---
+++ middleware/headers.js
@@ -0,0 +1,10 @@
+const headers = (req, res, next) => {
+	const origin = (req.headers.origin == 'http://localhost:3000') ? 'http://localhost:3000' : 'https://sunflower-land.com'
+	res.setHeader('Access-Control-Allow-Origin', origin)
+	res.setHeader('Access-Control-Allow-Methods', 'GET')
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	next()
+}
+
+module.exports = headers
--- routes/api/frogs.js
+++ routes/api/frogs.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const headers = require('../middleware/headers')
 
 let frogs = require('../../frogs_metadata.js');
 
-router.get("/", (req, res) => {
+router.get("/", headers, (req, res) => {
   res.json(frogs);
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", headers, (req, res) => {
   const found = frogs.some(frog => frog.edition === parseInt(req.params.id));
 
   if (found) {
