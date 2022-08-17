c841c5fa324b0322129ed21a7ec1f07060de1711
Tiffany Domugho <tiffany@domugho.com>
2022-08-17 13:21:35 +0800

    chore: change allow origin header to all

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,6 +1,6 @@
 const headers = (req, res, next) => {
-	const origin = (req.headers.origin == 'http://localhost:3000') ? 'http://localhost:3000' : 'https://sunflower-land.com'
-	res.setHeader('Access-Control-Allow-Origin', origin)
+	// const origin = (req.headers.origin == 'http://localhost:3000') ? 'http://localhost:3000' : 'https://sunflower-land.com'
+	res.setHeader('Access-Control-Allow-Origin', '*')
 	res.setHeader('Access-Control-Allow-Methods', 'GET')
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
 	res.setHeader('Access-Control-Allow-Credentials', true)
