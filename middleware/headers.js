45a98763c451a0ace785243e184224885b825e82
Tiffany Domugho <tiffany@domugho.com>
2022-08-17 13:49:39 +0800

    chore: update headers

--- middleware/headers.js
+++ middleware/headers.js
@@ -2,7 +2,7 @@ const headers = (req, res, next) => {
 	// const origin = (req.headers.origin == 'http://localhost:3000') ? 'http://localhost:3000' : 'https://sunflower-land.com'
 	res.setHeader('Access-Control-Allow-Origin', '*')
 	res.setHeader('Access-Control-Allow-Methods', 'GET')
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
 	res.setHeader('Access-Control-Allow-Credentials', true)
 	next()
 }
